80eceed61c40c179af08e5f0414991897d787edb
Kyle McLean <kyledmclean@gmail.com>
2020-04-23 17:46:13 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,11 +4,12 @@ def oxford_comma(array)
  final_item = array[-1]
  array.pop
  
- while array.size > 1
+ if array.size > 1
     array.join(", ")
     complete_item = array.join(", ") + ", and #{final_item}"
+    
   end
-  while array.size == 1
+  elseif array.size == 1
     complete_item = array + " and #{final_item}"
   end
 #  puts array
